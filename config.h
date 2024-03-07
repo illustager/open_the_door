4c5365cd81298d6b50ed34c56dfc387872627bcb
illustager <112761682+illustager@users.noreply.github.com>
2024-03-07 23:09:04 +0800

    rm all by mistake, try to reset

---
+++ config.h
@@ -0,0 +1,53 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+//------------------------------------------------------------舵机
+#define servoPin 		4       // 舵机引脚
+
+#define servoMinPulse 	500 	// 脉冲宽度范围 一般不用改
+#define servoMaxPulse 	2500
+
+#define servoDelayTime 	3 		// 秒
+
+#define servoOpenAngle  100		// 舵机开关门的角度
+#define servoCloseAngle 0
+
+//------------------------------------------------------------读卡器与用户数据
+// SS 和 RST 已固定 不要随便更改
+// MISO 和 MOSI 以及其它引脚只需按照引脚参考图正确连接即可 这些引脚是自动工作的
+#define rc522_SS_PIN  	12
+#define rc522_RST_PIN 	27
+
+// 用户数据见 user_data.h
+
+//------------------------------------------------------------音频
+#include "sound_data.h" // 音频数据
+const byte *sound_datas[] = {	sound_data::cn,
+							  	sound_data::en,
+							  	sound_data::jp,
+							  	sound_data::russian,
+							  	sound_data::guandong,
+							  	sound_data::sn,
+							  	sound_data::shanghai,
+							  	sound_data::eg	};
+
+#define audioPin 	25 			// 音频引脚
+#define audioNum 	8  			// 音频文件数量
+#define audioDatas 	sound_datas	// 音频数据
+
+//------------------------------------------------------------睡眠
+#define touchPin  	33 		// 触摸引脚
+#define threshold 	30 		// 触摸阈值
+#define wakeupTime 	10 		// 秒 醒来之后的工作时间
+#define delayTime 	1500  	// 毫秒 开完门之后的延时时间 时间过短则舵机来不及恢复到初始位置 
+
+//------------------------------------------------------------指示灯
+#define LEDPin 	 2 	 // 指示灯引脚
+
+#define LEDoff   0	 // 关闭时的 PWM 值
+#define LEDinfo1 25  // 一级信号 唤醒时
+#define LEDinfo2 255 // 二级信号 开门时 
+
+//------------------------------------------------------------
+
+#endif // CONFIG_H
--- open_the_door.ino
+++ open_the_door.ino
@@ -1,123 +1,115 @@
 #define DEBUG
 
-//-----------------------------------------------------------舵机
-#include "myServo.h"
-myServo my_servo(4, 500, 2500); // 舵机引脚 以及最小最大脉冲宽度
+//------------------------------------------------------------配置
+#include "config.h"
 
-#define servo_delay_time 3 // 此处单位为 s
+//------------------------------------------------------------舵机
+#include "myServo.h"
+myServo my_servo(servoPin, servoMinPulse, servoMaxPulse);
 
 //------------------------------------------------------------读卡器与用户数据
-// SS 和 RST 已固定 不要随便更改
-// MISO 和 MOSI 以及其它引脚只需按照引脚参考图正确连接即可 这些引脚是自动工作的
-#define rc522_SS_PIN  12
-#define rc522_RST_PIN 27 // 重置引脚
-
 #include "myIC.h"
 myIC my_ic(rc522_SS_PIN,rc522_RST_PIN);
 
 #include "user_data.h"
 
-//--------------------------------------------------------------------音频
+//------------------------------------------------------------音频
 #include "myAudio.h"
-myAudio my_audio(25); // 音频引脚
+myAudio my_audio(audioPin, audioNum, audioDatas);
 
-volatile bool is2play; // volatile !!!!!!!!!! 用于多线程之间的通信
+volatile bool is2play; 			// volatile !!!!!!!!!! 用于多线程之间的通信
 TaskHandle_t playHandle = NULL; // 多线程句柄
 
 void playTask(void*) { // 多线程任务函数 - 播放音频
-  static RTC_DATA_ATTR byte select = 0;
-  
-  while(true) {
-    if( is2play ) {
-      my_audio.play(select);
-      is2play = false;
-      ++select;
-    }
-  }
+	static RTC_DATA_ATTR byte select = 0;
+	while(true) {
+		if( is2play ) {
+			my_audio.play(select);
+			is2play = false;
+			++select;
+		}
+	}
 }
 
 //------------------------------------------------------------休眠
 #include <esp_sleep.h>
-// RTC_DATA_ATTR int bootCount = 0;
-#define touchPin  33
-#define threshold 30
-#define wakeup_work_time 10 // 此处单位为 s
 
-void callbackFun() { // 回调函数 必须实现
-  // Serial.println("good");
+void callbackFunc() { // 回调函数
+	// Serial.println("good");
 }
 
 //------------------------------------------------------------指示灯
-#define LEDPin 2
 
-//-------------------------------------------------------------------
+//------------------------------------------------------------
 
 
 void setup() {
 #ifdef DEBUG
-  Serial.begin(115200);
+	Serial.begin(115200);
 #endif
 
-  esp_sleep_enable_touchpad_wakeup();
-  // 在 esp32 中 断分为两种 写入不同位置
-  // 一种是 RTC 中断 如定时器中断 触摸中断
-  // 另一种是 GPIO 中断
-  touchAttachInterrupt(touchPin, callbackFun, threshold); // 引脚 回调函数 阈值
-  touchDetachInterrupt(touchPin); // 先解除中断 而后在 loop 中重新启用中断 以免多次触发中断
+	esp_sleep_enable_touchpad_wakeup();
+	// 在 esp32 中 断分为两种 写入不同位置
+	// 一种是 RTC 中断 如定时器中断 触摸中断
+	// 另一种是 GPIO 中断
+	touchAttachInterrupt(touchPin, callbackFunc, threshold); // 引脚 回调函数 阈值
+	touchDetachInterrupt(touchPin); // 先解除中断 而后在 loop 中重新启用中断 以免多次触发中断
 
-  pinMode(LEDPin, OUTPUT);
+	pinMode(LEDPin, OUTPUT);
+	analogWrite(LEDPin, LEDoff);
 
-  xTaskCreate(  playTask,         /* 任务函数 */
-                "play",           /* 任务名 */
-                8*1024,           /* 任务栈大小，根据需要自行设置*/
-                NULL,             /* 参数，入参为空 */
-                1,                /* 优先级 */
-                &playHandle );    /* 任务句柄 */
+	my_servo.work(servoCloseAngle); // 初始化舵机
 
-}
+	xTaskCreate(  playTask,         // 任务函数
+				  "play",           // 任务名称
+				  8*1024,           // 任务栈大小，根据需要自行设置
+				  NULL,             // 参数 入参为空
+				  1,                // 优先级
+				  &playHandle );    // 任务句柄
+
+} // setup
 
 void loop() {
 
-  // 实现 esp32 在唤醒后维持一段时间的运行
+// 实现 esp32 在唤醒后维持一段时间的运行
 #ifdef DEBUG
-  Serial.println("Running...");
+	Serial.println("Working...");
 #endif
-  analogWrite(LEDPin, 25);
+	analogWrite(LEDPin, LEDinfo1);
 
-  uint64_t startTime = millis();
-  while( millis() - startTime < wakeup_work_time * 1000 ) {
-    if( my_ic.readyet() ) {
-      uint32_t uid = my_ic.read();
-      bool check_flag = user_data::check(uid);
-      // my_screen.debug(uid, check_flag);
-      if(check_flag) {
-      #ifdef DEBUG
-        Serial.println("Welcome!");
-      #endif
-        analogWrite(LEDPin, 255);
-        is2play = true;
-        //---------------------------------------------------
-        my_servo.work(135);
-        delay(servo_delay_time * 1000);
-        //-----------------------------------------------------
-        my_servo.work(0);
-    
-        //-----------------------------------------
-        break;
-      }
-    }
-  }
+	uint64_t startTime = millis();
+	while( millis() - startTime < wakeupTime * 1000 ) {
+		if( my_ic.readyet() ) {
+			uint32_t uid = my_ic.read();
+			int check_flag = user_data::check(uid);
 
-  is2play = false;
+			if( check_flag >= 0 ) {
+			#ifdef DEBUG
+				Serial.println("Welcome!");
+			#endif
+				analogWrite(LEDPin, LEDinfo2);
+				is2play = true;
+			//---------------------------------------------------
+				my_servo.work(servoOpenAngle);
+				delay(servoDelayTime * 1000);
+			//-----------------------------------------
+				break;
+			}
+		}
+	}
+
+	
 #ifdef DEBUG
-  Serial.println("ESP32 will sleep now!");
+	Serial.println("ESP32 will sleep now!");
 #endif
-  analogWrite(LEDPin, 0);
-  delay(100);
-  touchAttachInterrupt(touchPin, callbackFun, threshold);
-  esp_deep_sleep_start();
-}
 
+	is2play = false;
+	my_servo.work(servoCloseAngle);
 
+	delay(delayTime);
 
-
+	analogWrite(LEDPin, LEDoff);
+	
+	touchAttachInterrupt(touchPin, callbackFunc, threshold);
+	esp_deep_sleep_start();
+} // loop
